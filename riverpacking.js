d1275df4a535197a8079af66d0251fca374e8546
James Russo <1jlrusso@gmail.com>
2018-07-25 02:39:04 -0700

    added window load function that calls functions that create a donut or bar chart for each product section based on the window width. create a donut-creating function and bar-creating function for each product section. these functions replaced the stand along objects that create a bar chart for each product section

--- riverpacking.js
+++ riverpacking.js
@@ -53,6 +53,8 @@ learnBtn.addEventListener("click", function(){
     scrollTop: $("#products-wrapper").offset().top - 50
   });
 });
+
+
 // Sign up and Login Sections
 var signInBtn = document.getElementById("sign-in-btn"),
     loginWrapper = document.getElementById("login-wrapper"),
@@ -90,6 +92,8 @@ orSignupBtn.addEventListener("click", function(){
   signUpWrapper.style.right = "0px";
   suArrowRight.style.display = "block";
 })
+
+
 /* Start of Location/Map Section */
 function initMap(){
   var coords = {lat: 38.126999, lng: -121.271420};
@@ -104,6 +108,8 @@ function initMap(){
   });
 }
 
+
+/* Start of Data Btn Variables and Assoc. Functions */
 var viewDataBtns = document.getElementsByClassName("data-btn");
 for(let i = 0; i < viewDataBtns.length; i++){
   viewDataBtns[i].addEventListener("click", () => {
@@ -112,6 +118,8 @@ for(let i = 0; i < viewDataBtns.length; i++){
   });
 };
 
+
+/* Start of Left Arrow Vars and Assoc. Functions */
 var leftArrows = document.getElementsByClassName("fa-arrow-left");
 for(let i = 0; i < leftArrows.length; i++){
   leftArrows[i].addEventListener("click", () => {
@@ -119,155 +127,257 @@ for(let i = 0; i < leftArrows.length; i++){
     gParent.style.left = "0";
   });
 };
-/* Start of Cherry Chart JS */
-var outputChart = document.getElementById("cherry-chart").getContext('2d');
-var barChart = new Chart(outputChart, {
-  type: 'bar',
-  data: {
-    labels: ["Brooks", "Royal Hazel", "Tulare", "Coral", "Chelan", "Bing", "Lapin", "Rainier"],
-    datasets: [{
-        label: 'Expected Output',
-        data: [150000, 50000, 100000, 200000, 10000, 400000, 70000, 30000],
-        backgroundColor: 'rgba(217, 136, 128, 0.2)',
-        borderWidth: 1,
-        borderColor: 'rgb(217, 136, 128)',
-        hoverBorderWidth: 3
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: '2018 Cherry Projections',
-      fontSize: 25
-    },
-    responsive: true,
-    maintainAspectRation: false,
-    legend: {
-      labels: {
-        fontColor: 'black'
-      }
-    },
-    tooltips: {
-      enabled: true,
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        },
-        scaleLabel: {
-          display: true,
-          labelString: '# of Cartons'
-        }
-      }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: 'Varities'
-        }
-      }]
-    }
+
+/* Start of Creating Bar or Donut Charts based on window width */
+window.addEventListener("load", function(){
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  if(windowWidth < 820){
+    createCherryDonut();
+    createGrapeDonut();
+    createPearDonut();
+  } else {
+    createCherryBar();
+    createGrapeBar();
+    createPearBar();
   }
 });
+
+
+/* Start of Cherry Chart JS */
+function createCherryDonut(){
+  var cherryDonutChart = c3.generate({
+    bindto: '#cherry-donut-chart',
+    data: {
+      columns: [
+        ['Brooks', 150000],
+        ['Royal Hazel', 50000],
+        ['Tulare', 100000],
+        ['Coral', 200000],
+        ['Chelan', 10000],
+        ['Bing', 400000],
+        ['Lapin', 70000],
+        ['Rainier', 30000]
+      ],
+      type: 'donut'
+    },      
+    donut: {
+      title: "2018 Cherry Projections",
+      label: {
+        format: function (value) {return value;}
+      }
+    }
+  });
+}
+
+function createCherryBar(){
+  var outputChart = document.getElementById("cherry-chart").getContext('2d');
+  var barChart = new Chart(outputChart, {
+    type: 'bar',
+    data: {
+      labels: ["Brooks", "Royal Hazel", "Tulare", "Coral", "Chelan", "Bing", "Lapin", "Rainier"],
+      datasets: [{
+          label: 'Expected Output',
+          data: [150000, 50000, 100000, 200000, 10000, 400000, 70000, 30000],
+          backgroundColor: 'rgba(217, 136, 128, 0.2)',
+          borderWidth: 1,
+          borderColor: 'rgb(217, 136, 128)',
+          hoverBorderWidth: 3
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '2018 Cherry Projections',
+        fontSize: 25
+      },
+      responsive: true,
+      maintainAspectRation: false,
+      legend: {
+        labels: {
+          fontColor: 'black'
+        }
+      },
+      tooltips: {
+        enabled: true,
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          },
+          scaleLabel: {
+            display: true,
+            labelString: '# of Cartons'
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Varities'
+          }
+        }]
+      }
+    }
+  });
+}
+
+
 
 /* Start of Grapes Chart */
-var outputChart = document.getElementById("grape-chart").getContext('2d');
-var barChart = new Chart(outputChart, {
-  type: 'bar',
-  data: {
-    labels: ["Cinsaut", "Gamay", "Mencia", "Mandilaria", "Bobal", "Ciliegiolo", "Scheurebe", "Aramon"],
-    datasets: [{
-        label: 'Expected Output',
-        data: [100000, 80000, 210000, 145000, 160000, 95000, 45000, 170000],
-        backgroundColor: 'rgba(46, 204, 113, 0.2)',
-        borderWidth: 1,
-        borderColor: 'rgb(46, 204, 113)',
-        hoverBorderWidth: 3
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: '2018 Grape Projections',
-      fontSize: 25
-    },
-    responsive: true,
-    maintainAspectRation: false,
-    legend: {
-      labels: {
-        fontColor: 'black'
+function createGrapeDonut(){
+  var grapeDonutChart = c3.generate({
+    bindto: '#grape-donut-chart',
+    data: {
+      columns: [
+        ['Cinsaut', 100000],
+        ['Gamay', 80000],
+        ['Mencia', 210000],
+        ['Mandilaria', 145000],
+        ['Bobal', 160000],
+        ['Ciliegiolo', 95000],
+        ['Scheurebe', 45000],
+        ['Aramon', 170000]
+      ],
+      type: 'donut'
+    },      
+    donut: {
+      title: "2018 Grape Projections",
+      label: {
+        format: function (value) {return value;}
       }
-    },
-    tooltips: {
-      enabled: true,
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        },
-        scaleLabel: {
-          display: true,
-          labelString: '# of Cartons'
-        }
-      }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: 'Varities'
-        }
-      }]
     }
-  }
-});
+  })
+}
+
+function createGrapeBar(){
+  var outputChart = document.getElementById("grape-chart").getContext('2d');
+  var barChart = new Chart(outputChart, {
+    type: 'bar',
+    data: {
+      labels: ["Cinsaut", "Gamay", "Mencia", "Mandilaria", "Bobal", "Ciliegiolo", "Scheurebe", "Aramon"],
+      datasets: [{
+          label: 'Expected Output',
+          data: [100000, 80000, 210000, 145000, 160000, 95000, 45000, 170000],
+          backgroundColor: 'rgba(46, 204, 113, 0.2)',
+          borderWidth: 1,
+          borderColor: 'rgb(46, 204, 113)',
+          hoverBorderWidth: 3
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '2018 Grape Projections',
+        fontSize: 25
+      },
+      responsive: true,
+      maintainAspectRation: false,
+      legend: {
+        labels: {
+          fontColor: 'black'
+        }
+      },
+      tooltips: {
+        enabled: true,
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          },
+          scaleLabel: {
+            display: true,
+            labelString: '# of Cartons'
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Varities'
+          }
+        }]
+      }
+    }
+  });
+}
+
+
 
 /* Start of Pear Chart */
-var outputChart = document.getElementById("pear-chart").getContext('2d');
-var barChart = new Chart(outputChart, {
-  type: 'bar',
-  data: {
-    labels: ["Bartletts", "Comice", "Sunsprite", "Seckel", "French Butter", "Anjou", "Starkrimson", "Asian"],
-    datasets: [{
-        label: 'Expected Output',
-        data: [30000, 50000, 100000, 15000, 60000, 130000, 200000, 180000],
-        backgroundColor: 'rgba(241, 196, 15, 0.2)',
-        borderWidth: 1,
-        borderColor: 'rgb(241, 196, 15)',
-        hoverBorderWidth: 3
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: '2018 Pear Projections',
-      fontSize: 25
-    },
-    responsive: true,
-    maintainAspectRation: false,
-    legend: {
-      labels: {
-        fontColor: 'black'
+function createPearDonut(){
+  var pearDonutChart = c3.generate({
+    bindto: '#pear-donut-chart',
+    data: {
+      columns: [
+        ['Bartletts', 30000],
+        ['Comice', 50000],
+        ['Sunsprite', 100000],
+        ['Seckel', 15000],
+        ['French Butter', 60000],
+        ['Anjou', 130000],
+        ['Starkrimson', 200000],
+        ['Asian', 180000]
+      ],
+      type: 'donut'
+    },      
+    donut: {
+      title: "2018 Pear Projections",
+      label: {
+        format: function (value) {return value;}
       }
-    },
-    tooltips: {
-      enabled: true,
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        },
-        scaleLabel: {
-          display: true,
-          labelString: '# of Cartons'
-        }
-      }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: 'Varities'
-        }
-      }]
     }
-  }
-});
+  })
+}
+
+function createPearBar(){
+  var outputChart = document.getElementById("pear-chart").getContext('2d');
+  var barChart = new Chart(outputChart, {
+    type: 'bar',
+    data: {
+      labels: ["Bartletts", "Comice", "Sunsprite", "Seckel", "French Butter", "Anjou", "Starkrimson", "Asian"],
+      datasets: [{
+          label: 'Expected Output',
+          data: [30000, 50000, 100000, 15000, 60000, 130000, 200000, 180000],
+          backgroundColor: 'rgba(241, 196, 15, 0.2)',
+          borderWidth: 1,
+          borderColor: 'rgb(241, 196, 15)',
+          hoverBorderWidth: 3
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '2018 Pear Projections',
+        fontSize: 25
+      },
+      responsive: true,
+      maintainAspectRation: false,
+      legend: {
+        labels: {
+          fontColor: 'black'
+        }
+      },
+      tooltips: {
+        enabled: true,
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          },
+          scaleLabel: {
+            display: true,
+            labelString: '# of Cartons'
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Varities'
+          }
+        }]
+      }
+    }
+  });
+}
+
